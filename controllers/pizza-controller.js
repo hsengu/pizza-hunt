62196f3f76357dcf4e3180d474f5050ff387f897
Hok S. Uy <hoksuy@gmail.com>
2021-10-11 16:09:49 -0700

    created comment model and controllers

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,6 +3,12 @@ const { Pizza } = require('../models');
 const pizzaController = {
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -12,8 +18,13 @@ const pizzaController = {
 
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
-                if(!dbPizzaData) {
+                if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
                 }
@@ -34,7 +45,7 @@ const pizzaController = {
     updatePizza({ params, body }, res) {
         Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
             .then(dbPizzaData => {
-                if(!dbPizzaData) {
+                if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
                 }
@@ -46,7 +57,7 @@ const pizzaController = {
     deletePizza({ params }, res) {
         Pizza.findOneAndDelete({ _id: params.id })
             .then(dbPizzaData => {
-                if(!dbPizzaData) {
+                if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
                 }
